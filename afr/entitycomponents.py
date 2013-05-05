ab3875dc22cf2f6fef87516dd710bc40fc6e7fc2
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-05-05 17:27:38 +1000

    implement item picking up
    and clean up AI so unusual events work again

--- afr/entitycomponents.py
+++ afr/entitycomponents.py
@@ -81,33 +81,43 @@ class AI(object):
     def run_ai(self):
         '''Stupid generic creature brain'''
         state = self.brainstate
+        me = self.owner
         if not self.owner.alive:
             return
         
+        # If we have a target, ensure it's valid
+        if 'target' in state and not state['target'].has_component('corporeal'):
+            # The target has vanished (eg item that was picked up)
+            del(state['target'])
+        
         # Find a target
         if 'target' not in state:
-            if self.owner.has_component('inventory'):
-                target = self.owner.find_nearby_pickupable()
+            if me.has_component('inventory'):
+                target = me.find_nearby_pickupable()
                 if target:
                     state['target'] = target
                     state['target_distance'] = 0
-            elif self.owner.has_component('fighter'):
-                target = self.owner.find_combat_target()
+                    state['target_action'] = 'pick_up'
+            elif me.has_component('fighter'):
+                target = me.find_combat_target()
                 if target:
                     state['target'] = target
                     state['target_distance'] = 1
+                    state['target_action'] = 'attack'
             else:
-                logging.debug("Can't find a target for %s" % self.owner.name)
+                logging.debug("Can't find a target for %s" % me.name)
         
         if 'target' in state:
             target = state['target']
             logging.debug("Using target: %s (%s, %s)" % (target.name, target.x, target.y))
+            
             path = afr.map.map.pathfind(self.owner.x, self.owner.y, target.x, target.y)
             if path == False:
                 logging.debug("Can't find path!")
             elif len(path) == state['target_distance']:
-                logging.debug("We're close enough to our destination.")
-                del(state['target'])
+                # We're close enough to our destination.
+                action = state['target_action']
+                getattr(me, action)(target) # call the function specified in target_action on ourselves, with the target as the only argument
             else:
                 dx = path[0].x - self.owner.x
                 dy = path[0].y - self.owner.y
@@ -117,9 +127,10 @@ class AI(object):
         else:
             # No target, wander around
             if random.random() > 0.5:
-                (dx, dy) = afr.map.map.pathfind_to(self.owner.x, self.owner.y, random.randint(0, afr.map.map.width), random.randint(0, afr.map.map.height))
-                self.owner.corporeal.x += dx
-                self.owner.corporeal.y += dy
+                (dx, dy) = (random.randint(-1, 1), random.randint(-1, 1))
+                if afr.map.map.tile_traversable(me.x + dx, me.y + dy):
+                    me.x += dx
+                    me.y += dy
                 
 class Inventory(object):
     '''Entity has an inventory'''
@@ -129,11 +140,13 @@ class Inventory(object):
     
     def pick_up(self, entity):
         self.inventory.append(entity)
-        entity.remove_component('corporeal')
+        entity.detach_component('corporeal')
+        logging.debug("%s picks up %s" % (self.owner.name, entity.name))
     
     def find_nearby_pickupable(self):
-        candidates = [e for e in afr.entity.entities if e.has_component('weapon')]
-        try:
-            return min(candidates, key=lambda c: afr.map.map.distance_between(self.owner.x, self.owner.y, c.x, c.y))
-        except ValueError: # min can't handle empty lists
-            return None        
+        candidates = [e for e in afr.entity.entities if (e.has_component('corporeal') and e.has_component('weapon'))]
+        if candidates:
+            best = min(candidates, key=lambda c: afr.map.map.distance_between(self.owner.x, self.owner.y, c.x, c.y))
+        else:
+            best = None
+        return best
