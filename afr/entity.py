bef3ad456c4ff104f9f3b0df1099c00576c7c044
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-05-05 17:27:01 +1000

    fix entity component detaching

--- afr/entity.py
+++ afr/entity.py
@@ -10,21 +10,21 @@ class Entity(object):
     def attach_component(self, component):
         name = component.__class__.__name__.lower()
         if name in self._components:
-            raise ValueError("Component by the name %s is already attached." % name)
+            raise ValueError("Component by the name %s is already attached to entity %s." % (name, self.name))
         else:
             logging.debug("Attaching %s to %s" % (name, self.name))
             component.owner = self
             self._components[name] = component
             for obj in component.export:
                 if hasattr(self, obj):
-                    raise AttributeError("Component exports %s which is already in use on this entity!" % obj)
+                    raise AttributeError("Component exports %s which is already in use on entity %s." % (obj, self.name))
                 logging.debug("Setting attribute %s" % obj)
                 setattr(self, obj, getattr(component, obj))
     
-    def detach_component(self, component):
+    def detach_component(self, name):
         '''Detach component by name'''
-        if not component not in self._components:
-            raise ValueError("No component named %s is attached." % component)
+        if name not in self._components.keys():
+            raise ValueError("Entity %s has no attached component named %s." % (self.name, name))
         else:
             component = self._components[name]
             del(self._components[name])
