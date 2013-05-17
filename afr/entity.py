bcab778f21318bee4d694ce41cbc2e1138398c77
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-05-17 13:10:24 +0800

    entity components attribute should be public

--- afr/entity.py
+++ afr/entity.py
@@ -3,18 +3,18 @@ import logging
 class Entity(object):
     def __init__(self, name, components):
         self.name = name
-        self._components = {}
+        self.components = {}
         for c in components:
             self.attach_component(c)
 
     def attach_component(self, component):
         name = component.__class__.__name__.lower()
-        if name in self._components:
+        if name in self.components:
             raise ValueError("Component by the name %s is already attached to entity %s." % (name, self.name))
         else:
             logging.debug("Attaching %s to %s" % (name, self.name))
             component.owner = self
-            self._components[name] = component
+            self.components[name] = component
             for obj in component.export:
                 if hasattr(self, obj):
                     raise AttributeError("Component exports %s which is already in use on entity %s." % (obj, self.name))
@@ -23,11 +23,12 @@ class Entity(object):
     
     def detach_component(self, name):
         '''Detach component by name'''
-        if name not in self._components.keys():
+        if name not in self.components.keys():
             raise ValueError("Entity %s has no attached component named %s." % (self.name, name))
         else:
-            component = self._components[name]
-            del(self._components[name])
+            logging.debug("Detaching %s from %s" % (name, self.name))
+            component = self.components[name]
+            del(self.components[name])
             for obj in component.export:
                 delattr(self, obj)
                 
