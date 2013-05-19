b1c91c276e82f8c0e3cad1fb10777eeac6d6cdaa
Alex Jurkiewicz <alex@jurkiewi.cz>
2013-05-19 17:44:12 +0800

    refactor entity health out of Fighter component into new Creature component

--- afr/entitycomponents.py
+++ afr/entitycomponents.py
@@ -28,16 +28,21 @@ class Weapon(EntityComponent):
         else:
             return cur
 
+class Creature(EntityComponent):
+    def __init__(self, max_hp):
+        self.max_hp = max_hp
+        self.current_hp = max_hp
+        self.alive = True
+
+        self.export = ['max_hp', 'current_hp', 'alive']
+
 class Fighter(EntityComponent):
     '''Entity can fight'''
-    def __init__(self, strength, hp, team):
+    def __init__(self, strength, team):
         self.strength = strength
-        self.hp = hp
         self.team = team
 
-        self.alive = True
-        
-        self.export = ['strength', 'hp', 'team', 'alive', 'find_combat_target', 'attack']
+        self.export = ['strength', 'team', 'find_combat_target', 'attack']
 
     def find_combat_target(self):
         candidates = [e for e in afr.entity.entities if (e.has_component('corporeal') and e.has_component('fighter') and e.alive and e.team != self.owner.team)]
@@ -52,22 +57,22 @@ class Fighter(EntityComponent):
 
         if attacker_str == 0 or defender_str > attacker_str:
             dmg = (defender_str - attacker_str)//2
-            self.hp -= dmg
+            self.owner.current_hp -= dmg
             print("{attacker} ({attackerhp} hp) attacks {defender} ({defenderhp} hp) but they block and counterattack for {dmg} damage!".format( \
-                attacker=self.owner.name, attackerhp=self.owner.hp, defender=defender.name, defenderhp=defender.hp, dmg=dmg))
+                attacker=self.owner.name, attackerhp=self.owner.current_hp, defender=defender.name, defenderhp=defender.current_hp, dmg=dmg))
         elif defender_str == 0 or attacker_str > defender_str:
             dmg = (attacker_str - defender_str)
-            defender.hp -= dmg
+            defender.current_hp -= dmg
             print("{attacker} ({attackerhp} hp) hits {defender} ({defenderhp} hp) for {dmg} damage!".format( \
-                attacker=self.owner.name, attackerhp=self.owner.hp, defender=defender.name, defenderhp=defender.hp, dmg=dmg))
+                attacker=self.owner.name, attackerhp=self.owner.current_hp, defender=defender.name, defenderhp=defender.current_hp, dmg=dmg))
         else:
             print("Mutual block!")
 
-        if self.hp <= 0:
+        if self.owner.current_hp <= 0:
             print("%s died!" % self.owner.name)
             self.alive = False
             self.owner.set_icon(afr.util.load_icon('skull-crossed-bones.png'))
-        if defender.hp <= 0:
+        if defender.current_hp <= 0:
             print("%s died!" % defender.name)
             defender.alive = False
             defender.set_icon(afr.util.load_icon('skull-crossed-bones.png'))
